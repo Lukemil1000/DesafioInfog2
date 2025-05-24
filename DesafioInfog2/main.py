e7ec5b01758e103499889c0ec854574c1b1c5ade
Lucas Pomar <lucaspomar@outlook.com>
2025-05-24 15:57:13 -0300

    Auth router

--- DesafioInfog2/main.py
+++ DesafioInfog2/main.py
@@ -1,77 +1,13 @@
-from fastapi import FastAPI, Depends, HTTPException
-from http import HTTPStatus
+from fastapi import FastAPI
 
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-
-from DesafioInfog2.models import User
-from DesafioInfog2.schemas.userSchemas import UserCreate, UserPublic, Token
-from DesafioInfog2.database import get_session
-from DesafioInfog2.securiry import hash_password, verify_password, create_access_token, get_token_user
+from DesafioInfog2.routers import auth
 
 app = FastAPI()
+
+app.include_router(auth.router)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/auth/register", status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserCreate, session: Session = Depends(get_session)):
-    existing_user = session.scalar(
-        select(User).where(
-            (User.username == user.username) | (User.email == user.email)
-        )
-    )
-
-    if existing_user:
-        if existing_user.username == user.username:
-            raise HTTPException(
-                status_code=HTTPStatus.CONFLICT,
-                detail="Username already registered"
-            )
-        elif existing_user.email == user.email:
-            raise HTTPException(
-                status_code=HTTPStatus.CONFLICT,
-                detail="Email already registered"
-            )
-
-    hashed_password = hash_password(user.password)
-
-    db_user = User(
-        username=user.username,
-        password=hashed_password,
-        email=user.email
-    )
-
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-
-    return db_user
-
-@app.post("/auth/login", status_code=HTTPStatus.OK, response_model=Token)
-def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
-    user = session.scalar(select(User).where(User.username == form_data.username))
-
-    if not user:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail="Incorrect username or password"
-        )
-
-    if not verify_password(form_data.password, user.password):
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            detail="Incorrect username or password"
-        )
-
-    token = create_access_token(data={"sub": user.username})
-
-    return {"access_token": token, "token_type": "bearer"}
-
-@app.post("/auth/refresh-token", status_code=HTTPStatus.OK, response_model=Token)
-def refresh_token(token_user: User = Depends(get_token_user)):
-    token = create_access_token(data={"sub": token_user.username})
-
-    return {"access_token": token, "token_type": "bearer"}
---
+++ DesafioInfog2/routers/auth.py
@@ -0,0 +1,74 @@
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from DesafioInfog2.database import get_session
+from DesafioInfog2.models import User
+from DesafioInfog2.schemas.userSchemas import UserPublic, UserCreate, Token
+from DesafioInfog2.securiry import hash_password, verify_password, create_access_token, get_token_user
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+
+@router.post("/register", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserCreate, session: Session = Depends(get_session)):
+    existing_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if existing_user:
+        if existing_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail="Username already registered"
+            )
+        elif existing_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail="Email already registered"
+            )
+
+    hashed_password = hash_password(user.password)
+
+    db_user = User(
+        username=user.username,
+        password=hashed_password,
+        email=user.email
+    )
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
+
+@router.post("/login", status_code=HTTPStatus.OK, response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
+    user = session.scalar(select(User).where(User.username == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Incorrect username or password"
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Incorrect username or password"
+        )
+
+    token = create_access_token(data={"sub": user.username})
+
+    return {"access_token": token, "token_type": "bearer"}
+
+@router.post("/refresh-token", status_code=HTTPStatus.OK, response_model=Token)
+def refresh_token(token_user: User = Depends(get_token_user)):
+    token = create_access_token(data={"sub": token_user.username})
+
+    return {"access_token": token, "token_type": "bearer"}
