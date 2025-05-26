977f51d74fb01cca64cd99ad0d6f65697c7d2a6f
Lucas Pomar <lucaspomar@outlook.com>
2025-05-25 23:56:14 -0300

    Not allow for order empty products

--- DesafioInfog2/schemas/orderSchemas.py
+++ DesafioInfog2/schemas/orderSchemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from DesafioInfog2.models import OrderState
 from DesafioInfog2.schemas.productSchemas import ProductPublic
@@ -10,12 +10,12 @@ class OrderPublic(BaseModel):
     id: int
     state: OrderState
     created_at: datetime
-    products: list[ProductPublic]
+    products: list[ProductPublic] = Field(min_length=1)
     client_id: int
 
 class OrderCreate(BaseModel):
     state: OrderState
-    products: list[int]
+    products: list[int] = Field(min_length=1)
     client_id: int
 
 class OrderList(BaseModel):
