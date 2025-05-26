f0ab4cfe66d33c7681cb16a5ce5ee34f1646e2c7
Lucas Pomar <lucaspomar@outlook.com>
2025-05-26 16:59:38 -0300

    Refactor test to use factory

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,13 +2,14 @@ import pytest
 from sqlalchemy import select
 
 from DesafioInfog2.models import User
+from tests.factories import UserFactory
 
 
 def test_create_user_in_database(session):
-    new_user = User(username="test", email="test@test.com", password="test_password")
+    new_user = UserFactory(username="aaa")
     session.add(new_user)
     session.commit()
 
-    found_user = session.scalar(select(User).where(User.username == "test"))
+    found_user = session.scalar(select(User).where(User.username == "aaa"))
 
-    assert found_user.username == "test"
+    assert found_user.username == "aaa"
