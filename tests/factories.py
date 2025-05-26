a9564c081db23f5392937e7f017fb623c8bf48d4
Lucas Pomar <lucaspomar@outlook.com>
2025-05-26 17:24:41 -0300

    Create client in database test

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,7 @@
 import factory
+from cpf_generator import CPF
 
-from DesafioInfog2.models import User
+from DesafioInfog2.models import User, Client
 
 
 class UserFactory(factory.Factory):
@@ -10,3 +11,11 @@ class UserFactory(factory.Factory):
     username = factory.sequence(lambda n: f"user{n}")
     email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
     password = factory.LazyAttribute(lambda obj: f'{obj.username}_password')
+
+class ClientFactory(factory.Factory):
+    class Meta:
+        model = Client
+
+    name = factory.sequence(lambda n: f"client{n}")
+    email = factory.LazyAttribute(lambda obj: f'{obj.name}@test.com')
+    cpf = CPF.format(CPF.generate())
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,8 +1,9 @@
 import pytest
+from cpf_generator import CPF
 from sqlalchemy import select
 
-from DesafioInfog2.models import User
-from tests.factories import UserFactory
+from DesafioInfog2.models import User, Client
+from tests.factories import UserFactory, ClientFactory
 
 
 def test_create_user_in_database(session):
@@ -13,3 +14,13 @@ def test_create_user_in_database(session):
     found_user = session.scalar(select(User).where(User.username == "aaa"))
 
     assert found_user.username == "aaa"
+
+def test_create_client_in_database(session):
+    cpf = CPF.format(CPF.generate())
+    new_client = ClientFactory(cpf=cpf)
+    session.add(new_client)
+    session.commit()
+
+    found_client = session.scalar(select(Client).where(Client.cpf == cpf))
+
+    assert found_client.cpf == cpf
